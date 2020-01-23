e244af1c1b3a6b081e00e846b9dc1d8be5c868fa
Amit Biswas <biswas1992@hotmail.com>
2020-01-23 03:14:37 -0500

    first commit for programmatic UI

---
+++ programmaticui-customCell/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  programmaticui-customCell
+//
+//  Created by Amit Biswas on 1/23/20.
+//  Copyright © 2020 Amit Biswas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
